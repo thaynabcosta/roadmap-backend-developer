90d1122a0c5d8e8036e0e0da78b2eea126ac9641
Thayna Costa <thaynab170@gmail.com>
2025-06-03 23:26:06 -0400

    feat: uso prático de arrays

---
+++ Python/exercicios-praticos/estrutura_de_dados/arrays.py
@@ -0,0 +1,17 @@
+# 🚀 Tarefa Direta
+#Crie uma list com os nomes das suas 5 comidas favoritas.
+
+#Crie um array com 5 números inteiros usando o módulo array.
+
+#Crie um np.array com 5 números e multiplique todos por 3.
+
+import array
+import numpy as np
+
+comidas = ["hambuguer artesanal", "sushi", "frango a parmegiana", "frango teryaki", "pipoca"]
+numeros = array.array("i", [1, 2, 3, 5, 7])
+numeros_multi = np.array([1, 2, 3, 5, 7])
+
+print(comidas)
+print(numeros)
+print(numeros_multi*2)
